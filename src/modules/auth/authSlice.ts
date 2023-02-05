8ae837b1e65171cb28e5c9bd82f1fa48e28d60ed
Andrei Pham <andrei.pham@gmail.com>
2023-02-05 19:15:45 +0200

    added snackbar error notification when unable to determine user authentication
    this is needed in order to provide any kind of user feedback when e.g. the server is malfunctioning

--- src/modules/auth/authSlice.ts
+++ src/modules/auth/authSlice.ts
@@ -1,9 +1,10 @@
 import type { PayloadAction } from "@reduxjs/toolkit";
 import { createSlice } from "@reduxjs/toolkit";
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import { User } from "./domain";
 import type { AppDispatch } from "../../app/store";
 import { setLoading } from "../general/generalSlice";
+import { enqueueSnackbar } from "notistack";
 
 export interface AuthState {
   user: User | null;
@@ -35,6 +36,12 @@ export const retrieveCurrentUser = () => async (dispatch: AppDispatch) => {
   try {
     const response = await axios.get("/api/auth/profile");
     dispatch(setUser(response.data));
+  } catch (e) {
+    if ((e as AxiosError)?.response?.status !== 401) {
+      enqueueSnackbar("Failed to determine user authentication. Please try again later.", {
+        variant: "error",
+      });
+    }
   } finally {
     dispatch(setLoading(false));
   }
