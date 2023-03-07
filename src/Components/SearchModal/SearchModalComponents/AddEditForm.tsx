722f511b00ca6c5a8acac5a610f8e9e2c0f449c4
unknown <drago.ispas@gmail.com>
2023-03-07 17:13:57 +0200

    addEditForm wip

--- src/Components/SearchModal/SearchModalComponents/AddEditForm.tsx
+++ src/Components/SearchModal/SearchModalComponents/AddEditForm.tsx
@@ -24,11 +24,13 @@ const AddEntryFormBox = styled(Box)<{ themeMode: string }>`
   alignitems: center;
   justify-content: center;
   background: ${(props) =>
-    props.themeMode === "dark" ? "rgba(238, 91, 70, 0.7)" : "rgba(238, 91, 70, 0.7)"};
+    props.themeMode === "dark" ? "rgba(238, 91, 70, 0.7)" : "rgba(238, 91, 70, 0.5)"};
   width: 100%;
   padding: 10px 0px;
   margin: 10px 0;
   border-radius: 10px;
+  box-shadow: 0 0 10px black;
+  margin: px;
 `;
 
 const Scrollable = styled(Box)`
@@ -153,6 +155,7 @@ export function AddEditForm() {
           display: "flex",
           alignItems: "center",
           flexDirection: "column",
+          padding: "0px 10px",
           // boxShadow: "0px 0px 5px black",
         }}
       >
